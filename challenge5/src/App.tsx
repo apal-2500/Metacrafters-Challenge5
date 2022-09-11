6cfc495df629808f9264d116290f429a5d55cce9
apal-2500 <pallas.anita@gmail.com>
2022-09-11 16:20:51 +1000

    Add src/App.tsx

--- challenge5/src/App.tsx
+++ challenge5/src/App.tsx
@@ -1,28 +1,24 @@
-import React from 'react';
+// importfunctionalities
+import React, { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {
-  Connection,
-  PublicKey,
-  clusterApiUrl,
-  Keypair,
-  LAMPORTS_PER_SOL,
-  Transaction,
-  sendAndConfirmTransaction,
-  SystemProgram,
-} from "@solana/web3.js";
-import {useEffect , useState } from "react";
+import { 
+  PublicKey, 
+  Transaction, 
+  Keypair, 
+  Connection, 
+  clusterApiUrl, 
+  LAMPORTS_PER_SOL, 
+  SystemProgram, 
+  sendAndConfirmTransaction 
+} from '@solana/web3.js';
+import { useEffect, useState } from 'react';
 
 // create types
-type DisplayEncoding = "utf8" | "hex";
+type DisplayEncoding = 'utf8' | 'hex';
 
-type PhantomEvent = "disconnect" | "connect" | "accountChanged";
-type PhantomRequestMethod =
-  | "connect"
-  | "disconnect"
-  | "signTransaction"
-  | "signAllTransactions"
-  | "signMessage";
+type PhantomEvent = 'disconnect' | 'connect' | 'accountChanged';
+type PhantomRequestMethod = 'connect' | 'disconnect' | 'signTransaction' | 'signAllTransactions' | 'signMessage';
 
 interface ConnectOpts {
   onlyIfTrusted: boolean;
@@ -34,10 +30,7 @@ interface PhantomProvider {
   isConnected: boolean | null;
   signTransaction: (transaction: Transaction) => Promise<Transaction>;
   signAllTransactions: (transactions: Transaction[]) => Promise<Transaction[]>;
-  signMessage: (
-    message: Uint8Array | string,
-    display?: DisplayEncoding
-  ) => Promise<any>;
+  signMessage: (message: Uint8Array | string, display?: DisplayEncoding) => Promise<any>;
   connect: (opts?: Partial<ConnectOpts>) => Promise<{ publicKey: PublicKey }>;
   disconnect: () => Promise<void>;
   on: (event: PhantomEvent, handler: (args: any) => void) => void;
@@ -45,123 +38,123 @@ interface PhantomProvider {
 }
 
 /**
-  * @description gets Phantom provider, if it exists
-  */
-  const getProvider = (): PhantomProvider | undefined => {
-    if ("solana" in window) {
-      // @ts-ignore
-      const provider = window.solana as any;
-      if (provider.isPhantom) return provider as PhantomProvider;
+ * @description gets Phantom provider, if it exists
+ */
+const getProvider = (): PhantomProvider | undefined => {
+  if ('solana' in window) {
+    // @ts-ignore
+    const provider = window.solana as any;
+    if (provider.isPhantom) return provider as PhantomProvider;
   }
 };
 
-//Step 1: "Create an account"
+window.Buffer = window.Buffer || require('buffer').Buffer;
+
 function App() {
   // create state variable for the provider
-  const [provider, setProvider] = useState<PhantomProvider | undefined>(
-    undefined
-  );
-  
-  //create state variable for from keypair
-  const [fromkey, setFromKey] = useState([] as any);
-  const fromPair = Keypair.fromSecretKey(Uint8Array.from(fromkey));
-  
+  const [provider, setProvider] = useState<PhantomProvider | undefined>(undefined);
+
   // create state variable for the wallet key
-  const [walletKey, setWalletKey] = useState<PhantomProvider | undefined>(
-    undefined
-    );
-  
+  const [walletKey, setWalletKey] = useState<PhantomProvider | undefined>(undefined);
+
+  //generated wallet key
+  const [secKey, setSecKey] = useState<any>();
+  const [fromKey, setpubKey] = useState('');
+  //wallet balance
+  const [balance, setBalance] = useState(0);
+  const [signature, setSignature] = useState('');
+
   // this is the function that runs whenever the component updates (e.g. render, refresh)
   useEffect(() => {
-        const provider = getProvider();
-  
-        // if the phantom provider exists, set this as the provider
-        if (provider) setProvider(provider);
-        else setProvider(undefined);
+    const provider = getProvider();
+
+    // if the phantom provider exists, set this as the provider
+    if (provider) setProvider(provider);
+    else setProvider(undefined);
   }, []);
 
-  const createWallet = async () =>{
-  // Create a new keypair
-    const step1kPair = Keypair.generate();
-    setFromKey(step1kPair)
-    console.log("Public Key of the generated Step 1 keypair", step1kPair.publicKey);
-    // Connect to the Devnet
-    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-    // Request airdrop of 2 SOL to the wallet
-    console.log("Airdropping some SOL to Step 1 wallet!");
-    const fromAirDropSignature = await connection.requestAirdrop(
-        new PublicKey(fromPair.publicKey),
-        2 * LAMPORTS_PER_SOL
-     );
-    // Latest blockhash (unique identifer of the block) of the cluster
-    let latestBlockHash = await connection.getLatestBlockhash();
+  /**
+   * @description prompts user to connect wallet if it exists.
+   * This function is called when the connect wallet button is clicked
+   */
 
-    // Confirm transaction using the last valid block height (refers to its time)
-    // to check for transaction expiration
-    await connection.confirmTransaction({
-        blockhash: latestBlockHash.blockhash,
-        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-        signature: fromAirDropSignature
-     });
-
-    console.log("Airdrop completed Step 1 Wallet");
-
-    //Get balance for Step 1:
-    let step1Balance = await connection.getBalance(new PublicKey(fromPair.publicKey));
-    console.log("Step 1 Balance:", step1Balance/LAMPORTS_PER_SOL)
-  };
-
-  //step 2: Connect to wallet:
-    
-//prompts user to connect wallet if it exists.
-//This function is called when the connect wallet button is clicked
+  //Connect Wallet
   const connectWallet = async () => {
     // @ts-ignore
     const { solana } = window;
 
-  // checks if phantom wallet exists
+    // checks if phantom wallet exists
     if (solana) {
       try {
-// connects wallet and returns response which includes the wallet public key
+        // connects wallet and returns response which includes the wallet public key
         const response = await solana.connect();
-
-        console.log('Step 2 wallet account ', response.publicKey.toString());
-// update walletKey to be the public key
+        console.log('wallet account ', response.publicKey.toString());
+        // update walletKey to be the public key
         setWalletKey(response.publicKey.toString());
       } catch (err) {
-    // { code: 4001, message: 'User rejected the request.' }
+        // { code: 4001, message: 'User rejected the request.' }
       }
     }
   };
 
-//Step 3: Transfer Sol from Step 1 account to Step 2 account:
-  const transferSol = async() => {
-    //@ts-ignore
+  //Create an account
+  const createAccount = async () => {
+    const newPair = Keypair.generate();
+    const fromKey = newPair.publicKey.toString();
+    const secKey = newPair.secretKey;
+    setSecKey(secKey);
+    setpubKey(fromKey);
+
+    //airdrop Sol
+    const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+    const fromAirDropSignature = await connection.requestAirdrop(
+        new PublicKey(fromKey),
+        2 * LAMPORTS_PER_SOL
+    );
+    let latestBlockHash = await connection.getLatestBlockhash();
+    // to check for transaction expiration
+    await connection.confirmTransaction({
+      blockhash: latestBlockHash.blockhash,
+      lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+      signature: fromAirDropSignature,
+    });
+
+  //Update Balance
+  const getWalletBalance = async () => {
+    const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+    const walletBalance = await connection.getBalance(new PublicKey(fromKey));
+    setBalance(walletBalance);
+  };
+    await getWalletBalance();
+}
+
+  //Transfer Sol
+  const transferSol = async () => {
+    // @ts-ignore
     const { solana } = window;
 
-    // check for phantom wallet
+    // checks if phantom wallet exists
     if (solana) {
-      const step2Wallet = await solana.connect();
-      const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-      var transaction = new Transaction().add(
-        SystemProgram.transfer({
-          fromPubkey: fromPair.publicKey,
-          toPubkey: step2Wallet.publicKey,
-          lamports: LAMPORTS_PER_SOL / 100
-        })
-      );
-      //Sign transaction
-      console.log(fromPair)
-        var signature = await sendAndConfirmTransaction(
-          connection,
-          transaction,
-          [fromPair]
+      try {
+        // connects wallet and returns response which includes the wallet public key
+        const response = await solana.connect();
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+        const transaction = new Transaction().add(
+          SystemProgram.transfer({
+            fromPubkey: new PublicKey(fromKey),
+            toPubkey: response.publicKey,
+            lamports: 2 * LAMPORTS_PER_SOL,
+          })
         );
-        console.log('Signature is ', signature);
+        // Sign transaction
+        const signature = await sendAndConfirmTransaction(connection, transaction, [Keypair.fromSecretKey(secKey)]);
+        setSignature(signature);
+      } catch (err) {
+        // { code: 4001, message: 'User rejected the request.' }
+        console.log('Insufficient Balance');
+      }
     }
   };
-  
-
 // HTML code for the app
   return (
     <div className="App">
@@ -176,11 +169,12 @@ function App() {
               fontWeight: "bold",
               borderRadius: "5px",
             }}
-            onClick={createWallet}
+            onClick={createAccount}
           >
             Create Solana Wallet
           </button>
         )}
+        <p> Wallet created and airdrop complete</p>
       {provider && !walletKey && (
           <button
             style={{
@@ -194,8 +188,10 @@ function App() {
             Connect Wallet
           </button>
         )}
-        {provider && walletKey && <p>Connected account: {!walletKey.toString()}</p> }
-      {provider && !walletKey && (
+        {provider && walletKey && 
+        <>
+        <p>Connected account: {!walletKey.toString()}</p> 
+      
           <button
             style={{
               fontSize: "16px",
@@ -207,8 +203,9 @@ function App() {
           >
             Transfer SOL
           </button>
-        )}
-        
+          </>
+        }
+      
         {!provider && (
           <p>
             No provider found. Install{" "}
